709c8ffae9e3fc5a1405074a2e63f2e15b82bf74
Aashutosh <ashutoshg1101@gmail.com>
2024-01-24 19:08:30 +0530

    commit : after 1st UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import {useState} from 'react';
+import {useState ,useRef} from 'react';
 
 
 function App() {
 
   const [originalUrl, setOriginalUrl] = useState('');
   const [shortedUrl , setShortedUrl] = useState('');
+
+  const [copySuccess, setCopySuccess] = useState('');
+  const textAreaRef = useRef(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -18,9 +21,19 @@ function App() {
       const response = await axios.post('http://localhost:8000/url', body);
       setShortedUrl(response.data.shortURL);
       console.log(response);
+      setCopySuccess('');
     } catch (error) {
       console.error('Error shortening URL:', error);
     }
+  };
+
+  function copyToClipboard(e) {
+    textAreaRef.current.select();
+    document.execCommand('copy');
+    // This is just personal preference.
+    // I prefer to not show the whole text area selected.
+    e.target.focus();
+    setCopySuccess('Copied!');
   };
 
   return (
@@ -36,8 +49,29 @@ function App() {
         />
       </label>
       <button type="submit">Shorten URL</button>
-      <input type='text' value={shortedUrl}></input>
+      {/* <input type='text' value={shortedUrl} ></input> */}
     </form>
+
+    <div>
+      <form>
+        <label>
+          Generated URL<br/>
+        <textarea
+          ref={textAreaRef}
+          value={shortedUrl}
+        />
+        {
+        /* Logical shortcut for only displaying the 
+            button if the copy command exists */
+        document.queryCommandSupported('copy') &&
+          <div>
+            <button onClick={copyToClipboard}>Copy</button> 
+            {copySuccess}
+          </div>
+        }
+        </label>
+      </form>
+    </div>
     </>
   );
 }
